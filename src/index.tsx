c3cc2101de44536fc895a1db938b73626cdf4727
Tom Moor <tom.moor@gmail.com>
2021-06-21 15:38:52 -0700

    dll bug

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import SDK from '@elasticprojects/abstract-cli';
 import { render } from 'react-dom';
 import App from './App';
+
+console.log(SDK);
 
 render(<App />, document.getElementById('root'));
